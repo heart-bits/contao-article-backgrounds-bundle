55426fd8cfe42be6ca350584b20c00b065a90045
Sascha Wustmann <hi@saschawustmann.com>
2024-07-04 10:45:53 +0200

    Add meta data

--- src/EventListener/GetImage.php
+++ src/EventListener/GetImage.php
@@ -5,16 +5,19 @@ namespace Heartbits\ContaoArticleBackgrounds\EventListener;
 use Contao\CoreBundle\Image\PictureFactoryInterface;
 use Contao\FilesModel;
 use Contao\StringUtil;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class GetImage
 {
     private $projectDir;
     private $pictureFactory;
+    private $requestStack;
 
-    public function __construct(string $projectDir, PictureFactoryInterface $pictureFactory)
+    public function __construct(string $projectDir, PictureFactoryInterface $pictureFactory, RequestStack $requestStack)
     {
         $this->pictureFactory = $pictureFactory;
         $this->projectDir = $projectDir;
+        $this->requestStack = $requestStack;
     }
 
     public function pushImageToTemplate($objTemplate, $arrData, $objModule)
@@ -23,6 +26,10 @@ class GetImage
             $rootDir = $this->projectDir;
             $objFile = FilesModel::findByUuid($objTemplate->background_picture);
             if ($objFile !== null || is_file($this->projectDir . '/' . $objFile->path)) {
+                $locale = $this->requestStack->getCurrentRequest()->getLocale();
+                $meta = StringUtil::deserialize($objFile->meta);
+
+                // Add image
                 $picture = $this->pictureFactory
                     ->create($rootDir . '/' . $objFile->path, StringUtil::deserialize($objTemplate->background_size));
                 $data = [
@@ -31,6 +38,24 @@ class GetImage
                         'sources' => $picture->getSources($rootDir),
                     ]
                 ];
+
+                // Add meta data
+                if ($locale && isset($meta[$locale])) {
+                    if ($meta[$locale]['caption']) {
+                        $data['caption'] = $meta[$locale]['caption'];
+                    }
+                    if ($meta[$locale]['link']) {
+                        $data['href'] = $meta[$locale]['link'];
+                    }
+                    if ($meta[$locale]['alt']) {
+                        $data['picture']['alt'] = $meta[$locale]['alt'];
+                    }
+                    if ($meta[$locale]['title']) {
+                        $data['picture']['title'] = $meta[$locale]['title'];
+                        $data['linkTitle'] = $meta[$locale]['title'];
+                    }
+                }
+
                 $objTemplate->background_picture = $data;
             }
         }
